2301ed4af3975c44534b75fb659b788b1692a8be
stephen-ross2 <stephen.ross2@outlook.com>
2024-11-26 13:44:12 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Security;
+
+namespace Assignment_4_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> count = new Dictionary<string, int>();
+
+            string[] input = ["a", "b", "c", "a", "c"];
+
+            foreach (string s in input)
+            {
+                if (!count.TryAdd(s, 1))
+                {
+                    count[s]++;
+                }
+            }
+
+            foreach (var kvp in count)
+            {
+                Console.WriteLine($"{kvp.Key}; {kvp.Value}");
+
+            }
+        }
+    }
+}
